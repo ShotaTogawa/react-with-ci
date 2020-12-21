0df441220a40190c9bd6741843c82d113283d2ba
ShotaTogawa <s.togawa0112@gmail.com>
2020-12-22 08:12:57 +0900

    add: grid component

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './MainContent.scss';
 import Slideshow from '../slide-show/SlideShow';
 import Paginate from '../paginate/Paginate';
+import Grid from '../grid/Grid';
 
 const MainContent = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -16,14 +17,29 @@ const MainContent = () => {
   }
   const images = [
     {
-      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8,
     },
     {
-      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8,
     },
     {
-      url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-    }
+      url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8,
+    },
+    {
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8,
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8,
+    },
+    {
+      url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8,
+    },
   ];
   return (
     <div className="main-content">
@@ -34,6 +50,7 @@ const MainContent = () => {
           <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
         </div>
       </div>
+      <Grid images={images} />
       {/* display grid component */}
     </div>
   );
